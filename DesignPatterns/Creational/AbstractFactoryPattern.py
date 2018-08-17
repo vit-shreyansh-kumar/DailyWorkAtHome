7ae822b2cfc45622b9b36fbdb49dbe507896a9e5
shreyansh <shreyansh.kumar@puresoftware.com>
2018-08-17 11:10:26 +0530

    Abstract Factory Design Pattern.

--- DesignPatterns/Creational/AbstractFactoryPattern.py
+++ DesignPatterns/Creational/AbstractFactoryPattern.py
@@ -1,6 +1,32 @@
 #Abstract Factory Design Pattern.
 
-from abc import ABCMeta , ABC
+from abc import ABCMeta , ABC, abstractmethod
 
-class Vehicle:
-    pass
+
+class VegPizza(metaclass=ABCMeta):
+
+    @abstractmethod
+    def prepare(self, VegPizza):
+        pass
+
+
+class NonVegPizza(metaclass=ABCMeta):
+
+    @abstractmethod
+    def serve(self, NonVegPizza):
+        pass
+
+
+class DeluxVegPizza(VegPizza):
+    def prepare(self):
+        print("Prepare", type(self).__name__)
+
+
+class ChickenPizza(NonVegPizza):
+    def serve(self):
+        print("Serve", type(self).__name__)
+
+
+class MexicanVegPizza(VegPizza):
+    def prepare(self):
+        print("Prepare", type(self).__name__)
