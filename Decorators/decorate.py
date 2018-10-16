d1819bda5e835a078d011b98213779cabd91e3db
Shreyansh Kumar <3535454+vit-shreyansh-kumar@users.noreply.github.com>
2018-10-16 15:18:57 +0530

    Update decorate.py

--- Decorators/decorate.py
+++ Decorators/decorate.py
@@ -1,18 +1,35 @@
 __about__ = """ Decorators example. """
 
 
-def decorate(func):
-    def wrap(fname,lname):
-        fname = "Fname modified."
-        lname = "Lname modified."
-        func(fname, lname)
-    return wrap
+def hello_decorator(func):
+    def inner1(a,b):
+        # getting the returned value
+        a = 10
+        b = 22
+        returned_value = func(a,b)
+        # returning the value to the original frame
+        return returned_value
+    return inner1
 
-if __name__ == "__main__":
+# adding decorator to the function
+@hello_decorator
+def add_two_numbers(a, b):
+    print("Inside the function")
+    print("Modified a =", a)
+    print("Modified b =", b)
+    return a+b
 
-    @decorate
-    def mains(fname,lname):
-        print(fname)
-        print(lname)
 
-    mains("YO","ZO")
+a, b = 1, 2
+
+# getting the value through return of the function
+print("Sum =", add_two_numbers(a, b))
+
+
+# ---Output---
+# before Execution
+# Inside the function
+# Modified a = 10
+# Modified b = 22
+# after Execution
+# Sum = 32
